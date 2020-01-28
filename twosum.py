dbf984ea8e6c17916040ff5f9d029a86c31b52c9
KJ Wilson <yourboykjdev@gmail.com>
2020-01-28 11:47:41 -0800

    slight adjustments

--- twosum.py
+++ twosum.py
@@ -1,11 +1,25 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+import os
+
+# Brute Force Solution
+def twoSum(nums, target):
         pos = 1
         for add in range(0, len(nums)-1):
-            print(add)
-            for addend in range(pos, len(nums)-1):
-                print(addend)
+            # print(f"add {nums[add]}")
+            for addend in range(pos, len(nums)):
+                # print(f"addend {addend, nums[addend]}")
                 if (nums[add] + nums[addend]) == target:
                     return [add, addend]
             pos += 1
-        return "No match found"
+
+# Dictionary Solution
+# Put number in dictionary, check if counterpart is there
+        
+
+if __name__ == "__main__":
+    # TEST 1
+    # nums = [3, 2, 4]
+    # print(twoSum(nums, 6))
+
+    # TEST 2
+    nums = [3, 4, 4, 7, 8, 9]
+    print(twoSum(nums, 20))
