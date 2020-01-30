056ee04c2854e9590fedf6b1751d8772f4498db7
KJ Wilson <yourboykjdev@gmail.com>
2020-01-30 15:38:27 -0800

    added pytest to twosum

--- twosum.py
+++ twosum.py
@@ -1,25 +1,31 @@
 import os
 
 # Brute Force Solution
+# Iterate through each value
+# Compare it to each value in array
+# Move range position to avoid redundant comparisons
+# O(nlogn) time
 def twoSum(nums, target):
         pos = 1
         for add in range(0, len(nums)-1):
-            # print(f"add {nums[add]}")
+            print(f"add {nums[add]}")
             for addend in range(pos, len(nums)):
-                # print(f"addend {addend, nums[addend]}")
+                print(f"addend {addend, nums[addend]}")
                 if (nums[add] + nums[addend]) == target:
-                    return [add, addend]
+                    return (nums[add], nums[addend])
+            print(pos)
             pos += 1
+        return None
 
 # Dictionary Solution
 # Put number in dictionary, check if counterpart is there
+# O(n) time
+def twoSum_dict(nums, target):
+    # number dictionary
+    numd = {}
+    for add in nums:
         
 
 if __name__ == "__main__":
-    # TEST 1
-    # nums = [3, 2, 4]
-    # print(twoSum(nums, 6))
-
-    # TEST 2
     nums = [3, 4, 4, 7, 8, 9]
-    print(twoSum(nums, 20))
+    print(twoSum(nums, 17))
---
+++ twosum_test.py
@@ -0,0 +1,19 @@
+import unittest
+from twosum import twoSum
+
+class TwoSumTest(unittest.TestCase):
+    # two sum returns first pair to equal target
+    def test_positive_integer_array(self):
+        nums = [3, 2, 4]
+        assert twoSum(nums, 6) == (2,4)
+        nums = [3, 4, 4, 7, 8, 9]
+        assert twoSum(nums, 17) == (8,9)
+    
+    def test_no_pair_found(self):
+        nums = [3, 2, 4]
+        assert twoSum(nums, 10) is None
+        nums = [3, 4, 4, 7, 8, 9]
+        assert twoSum(nums, 20) is None
+
+if __name__ == '__main__':
+    unittest.main()
