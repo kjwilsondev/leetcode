3718da90a042e55a65fbeb05fa805fe64d01fdae
KJ Wilson <yourboykjdev@gmail.com>
2020-01-30 15:53:30 -0800

    dictionary solution implemented

--- twosum.py
+++ twosum.py
@@ -1,10 +1,19 @@
 import os
 
-# Brute Force Solution
-# Iterate through each value
-# Compare it to each value in array
-# Move range position to avoid redundant comparisons
-# O(nlogn) time
+"""
+Brute Force Solution
+
+Input: array of numbers, target
+Output: first pair of numbers that add up to target
+
+Procedure:
+Iterate through each value
+Compare it to each value in array
+Move range position to avoid redundant comparisons
+
+Time Complexity:
+O(nlogn) time
+"""
 def twoSum(nums, target):
         pos = 1
         for add in range(0, len(nums)-1):
@@ -17,15 +26,39 @@ def twoSum(nums, target):
             pos += 1
         return None
 
-# Dictionary Solution
-# Put number in dictionary, check if counterpart is there
-# O(n) time
+"""
+Dictionary Solution
+
+Input: 
+array of numbers, target
+
+Output: 
+Array of tuples that add up to target
+
+Procedure:
+Iterate through each value
+Check if target counterpart is in dictionary
+Add value to dictionary
+
+Time Complexity:
+O(n) time
+"""
 def twoSum_dict(nums, target):
     # number dictionary
     numd = {}
-    for add in nums:
+    solutions = []
+    for add in nums: # O(n)
+        print(numd)
+        if (target - add) in numd: # O(1)
+            numd[target-add] -= 1
+            solutions.append((add, target-add))
+        if add in numd: # O(1)
+            numd[add] += 1
+        else:
+            numd[add] = 1
+    return solutions
         
 
 if __name__ == "__main__":
-    nums = [3, 4, 4, 7, 8, 9]
-    print(twoSum(nums, 17))
+    nums = [3, 4, 4, 7, 8, 9, 10]
+    print(twoSum_dict(nums, 17))
