da503a544e65b5de3087303af22f37e193c07870
KJ Wilson <yourboykjdev@gmail.com>
2020-02-03 02:03:38 -0800

    completed largest_value

--- largest_value.py
+++ largest_value.py
@@ -7,9 +7,11 @@ O(nlogn)
 def largestValue(nums, k):
     # sort_nums = sorted(nums)
     # return sort_nums[:k]
-    return sorted(nums)[:k]
+    return sorted(nums)[-k:]
 
 if __name__ == "__main__":
+    # TESTS
     nums = [3, 9, 4, 7, 8, 9]
-    # print(largestValue(nums, 3)) # [3, 4, 7]
-    print(largestValue2(nums, 3))
+    print(largestValue(nums, 3)) # [8, 9, 9]
+    nums = [5, 3, 6, 8, 2, 4, 7]
+    print(largestValue(nums, 4)) # [5, 6, 7, 8]
